dbc1657e85cb20035198263f98b8ee658af61e16
sohui <jei0308@naver.com>
2023-11-23 12:09:12 +0900

    서브페이지 헤더 오류 수정

--- subpage/product-list/js/sub.js
+++ subpage/product-list/js/sub.js
@@ -13,15 +13,15 @@ const headerHeight = header.getBoundingClientRect().height;
 const headerGnb = document.querySelector(".web-gnb");
 const webLogo = document.querySelector(".web-logo");
 
-window.addEventListener("scroll", () => {
-    if (window.scrollY > headerHeight) {
-        header.setAttribute("style", "background: white; border-bottom: 1px solid #ddd; transition: .2s all;");
-        headerGnb.setAttribute("style", "color: #000;");
-        webLogo.setAttribute("style", "color: #000;");
+$(window).scroll(function () {
+    let winTop = $(window).scrollTop();
+
+    if (winTop > 1) {
+        $('.web-header').css({ 'background-color': '#fff', 'border-bottom': '1px solid #ddd', 'transition': '.2s all' });
+        $('.web-gnb, .web-logo').css({ 'color': '#000' });
     } else {
-        header.setAttribute("style", "background: transparent; color: #000;");
-        headerGnb.setAttribute("style", "color: #fff;");
-        webLogo.setAttribute("style", "color: #fff;");
+        $('.web-header').css({ 'background-color': 'rgba(0,0,0,0)', 'border-bottom': 'none', 'color': '#fff', 'transition': '.2s all' });
+        $('.web-gnb, .web-logo').css({ 'color': '#fff' });
     }
 });
 
